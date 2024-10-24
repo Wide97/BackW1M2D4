6695dfae6b1d2d0b5400b6812f87163d99ce0046
Wide97 <mwidesott@gmail.com>
2024-10-24 16:01:50 +0200

    repository

--- src/main/java/marcowidesott/BackW1M2D4/repositories/BibiteRepository.java
+++ src/main/java/marcowidesott/BackW1M2D4/repositories/BibiteRepository.java
@@ -1,4 +1,7 @@
 package marcowidesott.BackW1M2D4.repositories;
 
-public class BibiteRepository {
+import marcowidesott.BackW1M2D4.entities.Bibite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BibiteRepository extends JpaRepository<Bibite, Long> {
 }
--- src/main/java/marcowidesott/BackW1M2D4/repositories/PizzaRepository.java
+++ src/main/java/marcowidesott/BackW1M2D4/repositories/PizzaRepository.java
@@ -1,4 +1,7 @@
 package marcowidesott.BackW1M2D4.repositories;
 
-public class PizzaRepository {
+import marcowidesott.BackW1M2D4.entities.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Long> {
 }
