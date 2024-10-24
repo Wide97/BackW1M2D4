100595958f19edf8febf5673d26b7f14aa8950c6
Wide97 <mwidesott@gmail.com>
2024-10-24 15:04:55 +0200

    Inizialize

---
+++ src/main/java/marcowidesott/BackW1M2D4/BackW1M2D4Application.java
@@ -0,0 +1,13 @@
+package marcowidesott.BackW1M2D4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackW1M2D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackW1M2D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/marcowidesott/BackW1M2D4/BackW1M2D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcowidesott.BackW1M2D4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackW1M2D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
